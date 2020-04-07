d3c9b4c62c8da93992d7a3b8a652624ff1cd45ab
yulu.kong <yulu.kong@ubtrobot.com>
2020-04-07 15:39:38 +0800

    init project

---
+++ ios/oc/KH264Kit/KH264Kit/KH264Kit.h
@@ -0,0 +1,19 @@
+//
+//  KH264Kit.h
+//  KH264Kit
+//
+//  Created by yulu kong on 2020/4/7.
+//  Copyright © 2020 yulu kong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KH264Kit.
+FOUNDATION_EXPORT double KH264KitVersionNumber;
+
+//! Project version string for KH264Kit.
+FOUNDATION_EXPORT const unsigned char KH264KitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KH264Kit/PublicHeader.h>
+
+
